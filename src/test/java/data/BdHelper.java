003654bdf63c41dfa0f5e7d9ad622f1e5712e3c4
Ira Anichkova <anichkova2016@yandex.ru>
2021-07-12 14:36:44 +0300

    changeFile BdHelper

--- src/test/java/data/BdHelper.java
+++ src/test/java/data/BdHelper.java
@@ -7,41 +7,43 @@ import org.apache.commons.dbutils.handlers.ScalarHandler;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 public class BdHelper {
     private static final QueryRunner runner = new QueryRunner();
-    private final Connection conn = getConnect();
 
-    @SneakyThrows
-    private Connection getConnect() {
-        return DriverManager.getConnection(
-                System.getProperty("spring.datasource.url"),
-                System.getProperty("spring.datasource.username"),
-                System.getProperty("spring.datasource.password")
-        );
+       public static Connection getConnection() throws SQLException {
+        String dbUrl = System.getProperty("db.url");
+        String login = System.getProperty("login");
+        String password = System.getProperty("password");
+        final Connection connection = DriverManager.getConnection(dbUrl, login, password);
+        return connection;
     }
+
+
 
     @SneakyThrows
     public String getPaymentStatus() {
         val status = "SELECT status FROM payment_entity ORDER BY created DESC";
-        return runner.query(conn, status, new ScalarHandler<>());
+        return runner.query(getConnection(), status, new ScalarHandler<>());
     }
 
     @SneakyThrows
     public Integer getPaymentAmount() {
         val amount = "SELECT amount FROM payment_entity ORDER BY created DESC";
-        return runner.query(conn, amount, new ScalarHandler<>());
+        return runner.query(getConnection(), amount, new ScalarHandler<>());
     }
 
     @SneakyThrows
     public String getCreditRequestStatus() {
         val status = "SELECT status FROM credit_request_entity ORDER BY created DESC";
-        return runner.query(conn, status, new ScalarHandler<>());
+        return runner.query(getConnection(), status, new ScalarHandler<>());
     }
 
     @SneakyThrows
     public String getCreditId() {
         val id = "SELECT credit_id FROM order_entity ORDER BY created DESC";
-        return runner.query(conn, id, new ScalarHandler<>());
+        return runner.query(getConnection(), id, new ScalarHandler<>());
     }
 }
